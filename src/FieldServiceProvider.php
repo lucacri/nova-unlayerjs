226f8287db7d8e29faea48aaec848daf57a8a786
Luca Critelli <lucacri@gmail.com>
2018-09-18 12:54:02 -0400

    Changed the repo path

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Lucacri\Unlayerjs;
+namespace Lucacri\NovaUnlayerjs;
 
 use Laravel\Nova\Nova;
 use Laravel\Nova\Events\ServingNova;
--- src/Unlayerjs.php
+++ src/Unlayerjs.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Lucacri\Unlayerjs;
+namespace Lucacri\NovaUnlayerjs;
 
 use Laravel\Nova\Fields\Code;
 
